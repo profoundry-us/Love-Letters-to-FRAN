b8dfd1eefd1f65beea9dbc5486675292fbfb8f2b
Topher Fangio <fangiotophia@gmail.com>
2012-02-14 12:37:14 -0600

    Fixed typo.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,7 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Fran::Applicaiton.config.session_store = :cookie_store
+Fran::Application.config.session_store = :cookie_store
 Fran::Application.config.session = {
   :key => '_omniauth_fran_session',
   :domain => nil,
